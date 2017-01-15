bd698803f86a1ba0f68c31036971b13e1abc908d
Ivan Protsyuk <iprotsyuk@gmail.com>
2017-01-16 00:49:12 +0100

    Switched OS X installation script to a Miniconda Python distribution instead of the built-in one

--- installer/mac_installer.sh
+++ installer/mac_installer.sh
@@ -1,8 +1,8 @@
 echo
 echo
-echo Welcome to installation of Optimus version 0.1.2017.01.14!
+echo Welcome to installation of Optimus version 0.1.2017.01.15!
 echo
-echo The installer will download about 2.1 GB of data so it will take some time depending on the speed of your internet connection.
+echo The installer will download about 2.4 GB of data so it will take some time depending on the speed of your internet connection.
 echo
 echo If you want to cancel the installation, press Ctrl+C.
 echo
@@ -14,15 +14,34 @@ echo Creating temporary directory...
 tmpDir=/tmp/OptimusInstaller
 rm -rf $tmpDir
 mkdir -p $tmpDir
+echo
 
-echo Installing necessary Python packages...
-easy_install pip > /dev/null
-pip install -q numpy==1.11.2 six==1.10.0 pandas==0.19.0 protobuf==2.6.1 pyopenms==2.0 pyMSpec==0.1
+echo Downloading Python installer...
+minicondaInstallScriptName=miniconda.sh
+(cd $tmpDir; curl "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh" > $minicondaInstallScriptName)
+echo
+
+echo Installing Python...
+echo
+minicondaInstallerPath=$tmpDir/$minicondaInstallScriptName
+pythonInstallationDir=$HOME/OptimusMiniconda
+rm -rf $pythonInstallationDir
+bash $minicondaInstallerPath -b -p $pythonInstallationDir
+echo
+
+pythonExeDir=$pythonInstallationDir/bin
+export PATH=$pythonExeDir:$PATH
+echo "export PATH="$pythonExeDir:$PATH"" > ~/.bash_profile
+
+echo Installing additional Python packages...
+$pythonExeDir/pip install -q numpy==1.11.2 six==1.10.0 pandas==0.19.0 protobuf==2.6.1 pyopenms==2.0 pyMSpec==0.1
+echo
 
 echo Downloading KNIME installer...
 knimeVersion=3.3.1
-(cd $tmpDir; curl -O "https://download.knime.org/analytics-platform/macosx/knime-full_"$knimeVersion".app.macosx.cocoa.x86_64.dmg")
-knimeInstallerPath=$tmpDir"/knime-full_"$knimeVersion".app.macosx.cocoa.x86_64.dmg"
+knimeImageName=knime.dmg
+(cd $tmpDir; curl "https://download.knime.org/analytics-platform/macosx/knime-full_"$knimeVersion".app.macosx.cocoa.x86_64.dmg" > $knimeImageName)
+knimeInstallerPath=$tmpDir/$knimeImageName
 
 echo
 echo Installing KNIME...
@@ -36,3 +55,4 @@ rm -rf $tmpDir
 
 echo Installation has finished successfully. You can run KNIME from \"Applications\".
 echo
+
